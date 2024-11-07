74a8dd930664bfe30b7f50c261314bb667bd498d
Clinton Ilupeju <clinton@Clintons-MacBook-Air.local>
2024-11-07 23:29:40 +0000

    pushing to repository

---
+++ clint2.py
@@ -0,0 +1,14 @@
+def simple_multiplication(my_data):
+    """
+
+    Doing a simple code and checking if updates are occurring
+    :return:
+
+    """
+
+    total = my_data + my_data
+    return total
+
+
+x = int(5)
+output = print(simple_multiplication(x))
